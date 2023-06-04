3a1d3bb02a45a477105d7a686563bd292947fa78
Francesco <frank.xdddf@gmail.com>
2023-06-04 17:25:55 +0200

    1.3

--- script.js
+++ script.js
@@ -45,3 +45,41 @@ window.addEventListener("scroll", function () {
     fixedDiv.classList.remove("hide");
   }
 });
+
+/*=================== icon navbar ==================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/*====================== active link ====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header ul a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header ul a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*====================== remove icons and navbar ====================*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
